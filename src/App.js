d62f456739138820a6fe6a47590226ba9f7038b8
RitikaShrestha-me <shrestharitika189@gmail.com>
2021-06-19 01:57:49 +0530

    imported antd package

--- src/App.js
+++ src/App.js
@@ -1,33 +1,16 @@
 import "./App.css";
 import Header from "./Components/Header/Header";
-import NewInvoice from "./Components/NewInvioce/NewInvoice";
-import { Invoices } from "./Components/Invoices/Invoices";
-import { Footer } from "./Components/Footer/Footer";
+import Home from "./Components/home/home";
+// import NewInvoice from "./Components/NewInvioce/NewInvoice";
+// import { Invoices } from "./Components/Invoices/Invoices";
+// import { Footer } from "./Components/Footer/Footer";
+import "antd/dist/antd.css"; // or 'antd/dist/antd.less';
 
 function App() {
-  let invoiceDetails = [
-    {
-      id: 101,
-      title: "Wood",
-      date: "Jan 21, 2021",
-    },
-    {
-      id: 102,
-      title: "Tiles",
-      date: "Jun 21, 2021",
-    },
-    {
-      id: 103,
-      title: "Labour",
-      date: "Jun 25, 2021",
-    },
-  ];
   return (
     <div className="App">
       <Header className="Header" />
-      <Invoices invoiceDetails={invoiceDetails} />
-      <NewInvoice />
-      <Footer />
+      <Home />
     </div>
   );
 }
---
+++ src/Components/home/home.js
@@ -0,0 +1,11 @@
+import InvoiceTable from "../invoice-table/invoice-table";
+import { Component } from "react";
+
+class Home extends Component {
+  state = {};
+  render() {
+    return <InvoiceTable />;
+  }
+}
+
+export default Home;
